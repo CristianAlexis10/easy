4f7eac97ac88f9485941d658d8fa530b41c49375
julian <jdgiraldo08@misena.edu.co>
2018-06-12 13:48:09 -0500

    wasasa

--- views/include/scope.leftMenu.php
+++ views/include/scope.leftMenu.php
@@ -1,7 +1,6 @@
 <div class="leftMenu">
   <ul>
-    <!--<a href="#">--><li><i class="fas fa-user"></i> modulo</li><!--</a>-->
-    <!--<a href="#">--><li><i class="fas fa-user"></i> modulo</li><!--</a>-->
-    <!--<a href="#">--><li><i class="fas fa-user"></i> modulo</li><!--</a>-->
+    <a href="#"><li><i class="fas fa-user"></i> usuario</li></a>
+    <a href="#"><li><i class="fas fa-user"></i> asistencia</li></a>
   </ul>
 </div>
--- views/include/scope.topMenu.php
+++ views/include/scope.topMenu.php
@@ -3,6 +3,6 @@
   <ul>
       <!-- <a href="#"> --><li><i class="far fa-question-circle"></i></li><!-- </a> -->
       <!-- <a href="#"> --><li><i class="fas fa-lock"></i></li><!-- </a> -->
-      <!-- <a href="#"> --><li><i class="fas fa-sign-out-alt"></i></li><!-- </a> -->
+      <a href="finalizar"><li><i class="fas fa-sign-out-alt"></i></li></a>
   </ul>
 </div>
