f9b3e97ab44245069f71a0ddfc8dd5df3c857119
CristianAlexis10 <cristian1020011@gmail.com>
2018-06-13 20:24:29 -0500

    ukjh

--- easy.sql
+++ easy.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-06-2018 a las 23:52:12
--- Versión del servidor: 10.1.32-MariaDB
--- Versión de PHP: 7.2.5
+-- Tiempo de generación: 14-06-2018 a las 03:23:54
+-- Versión del servidor: 10.1.8-MariaDB
+-- Versión de PHP: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -45,6 +43,139 @@ INSERT INTO `acceso` (`acc_token`, `acc_contra`, `acc_codigo`, `usu_codigo`) VAL
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `actividad`
+--
+
+CREATE TABLE `actividad` (
+  `id_act` int(11) NOT NULL,
+  `nom_act` varchar(100) DEFAULT NULL,
+  `fecha_realizacion` date NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fin` time NOT NULL,
+  `id_ins` int(11) NOT NULL,
+  `id_ficha` bigint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `aprendiz`
+--
+
+CREATE TABLE `aprendiz` (
+  `id_apre` int(11) NOT NULL,
+  `usu_codigo` int(11) NOT NULL,
+  `fic_codigo` int(11) NOT NULL,
+  `fecha_registro` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `aprendizxficha`
+--
+
+CREATE TABLE `aprendizxficha` (
+  `id_aprendiz` int(11) NOT NULL,
+  `id_ficha` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asistencia`
+--
+
+CREATE TABLE `asistencia` (
+  `id_actividad` int(11) NOT NULL,
+  `id_apren` int(11) NOT NULL,
+  `hora_entrada` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ficha`
+--
+
+CREATE TABLE `ficha` (
+  `id_ficha` bigint(20) NOT NULL,
+  `nom_ficha` varchar(100) DEFAULT NULL,
+  `id_jor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `instructor`
+--
+
+CREATE TABLE `instructor` (
+  `id_ins` int(11) NOT NULL,
+  `usu_codigo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `instructor`
+--
+
+INSERT INTO `instructor` (`id_ins`, `usu_codigo`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `instructor_ficha`
+--
+
+CREATE TABLE `instructor_ficha` (
+  `id_ins` int(11) DEFAULT NULL,
+  `id_ficha` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jornada`
+--
+
+CREATE TABLE `jornada` (
+  `id_jor` int(11) NOT NULL,
+  `nom_jor` varchar(20) DEFAULT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fin` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `jornada`
+--
+
+INSERT INTO `jornada` (`id_jor`, `nom_jor`, `hora_inicio`, `hora_fin`) VALUES
+(1, 'diurna', '00:00:00', '00:00:00'),
+(2, 'mixta', '00:00:00', '00:00:00'),
+(3, 'nocturna', '00:00:00', '00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol_id`
+--
+
+CREATE TABLE `rol_id` (
+  `rol_id` int(11) NOT NULL,
+  `rol_nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `rol_id`
+--
+
+INSERT INTO `rol_id` (`rol_id`, `rol_nombre`) VALUES
+(1, 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -58,15 +189,17 @@ CREATE TABLE `usuario` (
   `usu_correo` varchar(100) NOT NULL,
   `usu_celular` bigint(20) NOT NULL,
   `ciu_codigo` int(11) NOT NULL,
-  `usu_direccion` varchar(100) NOT NULL
+  `usu_direccion` varchar(100) NOT NULL,
+  `rol_id` int(11) NOT NULL,
+  `usu_estado` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`usu_codigo`, `usu_identificacion`, `usu_nombre`, `usu_nombre2`, `usu_apellido`, `usu_apellido2`, `usu_correo`, `usu_celular`, `ciu_codigo`, `usu_direccion`) VALUES
-(1, 9904, 'Cristian', '', 'lopera', '', 'dompi@gmail.com', 1231232, 1, 'sadas');
+INSERT INTO `usuario` (`usu_codigo`, `usu_identificacion`, `usu_nombre`, `usu_nombre2`, `usu_apellido`, `usu_apellido2`, `usu_correo`, `usu_celular`, `ciu_codigo`, `usu_direccion`, `rol_id`, `usu_estado`) VALUES
+(1, 9904, 'Cristian', '', 'lopera', '', 'dompi@gmail.com', 1231232, 1, 'sadas', 1, '');
 
 --
 -- Índices para tablas volcadas
@@ -76,26 +209,110 @@ INSERT INTO `usuario` (`usu_codigo`, `usu_identificacion`, `usu_nombre`, `usu_no
 -- Indices de la tabla `acceso`
 --
 ALTER TABLE `acceso`
+  ADD PRIMARY KEY (`acc_token`),
   ADD KEY `usu_codigo` (`usu_codigo`),
   ADD KEY `acc_codigo` (`acc_codigo`);
+
+--
+-- Indices de la tabla `actividad`
+--
+ALTER TABLE `actividad`
+  ADD PRIMARY KEY (`id_act`),
+  ADD KEY `id_ins` (`id_ins`),
+  ADD KEY `id_ficha` (`id_ficha`);
+
+--
+-- Indices de la tabla `aprendiz`
+--
+ALTER TABLE `aprendiz`
+  ADD PRIMARY KEY (`id_apre`),
+  ADD KEY `usu_codigo` (`usu_codigo`),
+  ADD KEY `fic_codigo` (`fic_codigo`);
+
+--
+-- Indices de la tabla `aprendizxficha`
+--
+ALTER TABLE `aprendizxficha`
+  ADD KEY `id_aprendiz` (`id_aprendiz`),
+  ADD KEY `id_ficha` (`id_ficha`);
+
+--
+-- Indices de la tabla `asistencia`
+--
+ALTER TABLE `asistencia`
+  ADD KEY `id_actividad` (`id_actividad`),
+  ADD KEY `id_apren` (`id_apren`);
+
+--
+-- Indices de la tabla `ficha`
+--
+ALTER TABLE `ficha`
+  ADD PRIMARY KEY (`id_ficha`),
+  ADD KEY `id_jor` (`id_jor`);
+
+--
+-- Indices de la tabla `instructor`
+--
+ALTER TABLE `instructor`
+  ADD PRIMARY KEY (`id_ins`),
+  ADD KEY `usu_codigo` (`usu_codigo`);
+
+--
+-- Indices de la tabla `instructor_ficha`
+--
+ALTER TABLE `instructor_ficha`
+  ADD KEY `id_ins` (`id_ins`),
+  ADD KEY `id_ficha` (`id_ficha`);
+
+--
+-- Indices de la tabla `jornada`
+--
+ALTER TABLE `jornada`
+  ADD PRIMARY KEY (`id_jor`);
+
+--
+-- Indices de la tabla `rol_id`
+--
+ALTER TABLE `rol_id`
+  ADD PRIMARY KEY (`rol_id`);
 
 --
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`usu_codigo`),
-  ADD KEY `ciu_codigo` (`ciu_codigo`);
+  ADD KEY `ciu_codigo` (`ciu_codigo`),
+  ADD KEY `rol_id` (`rol_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `actividad`
+--
+ALTER TABLE `actividad`
+  MODIFY `id_act` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `aprendiz`
+--
+ALTER TABLE `aprendiz`
+  MODIFY `id_apre` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `instructor`
+--
+ALTER TABLE `instructor`
+  MODIFY `id_ins` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `rol_id`
+--
+ALTER TABLE `rol_id`
+  MODIFY `rol_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `usu_codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
 --
 -- Restricciones para tablas volcadas
 --
@@ -105,7 +322,58 @@ ALTER TABLE `usuario`
 --
 ALTER TABLE `acceso`
   ADD CONSTRAINT `acceso_ibfk_1` FOREIGN KEY (`usu_codigo`) REFERENCES `usuario` (`usu_codigo`);
-COMMIT;
+
+--
+-- Filtros para la tabla `actividad`
+--
+ALTER TABLE `actividad`
+  ADD CONSTRAINT `actividad_ibfk_1` FOREIGN KEY (`id_ins`) REFERENCES `instructor_ficha` (`id_ins`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `aprendiz`
+--
+ALTER TABLE `aprendiz`
+  ADD CONSTRAINT `aprendiz_ibfk_1` FOREIGN KEY (`usu_codigo`) REFERENCES `usuario` (`usu_codigo`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `aprendiz_ibfk_2` FOREIGN KEY (`id_apre`) REFERENCES `asistencia` (`id_apren`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `aprendizxficha`
+--
+ALTER TABLE `aprendizxficha`
+  ADD CONSTRAINT `aprendizxficha_ibfk_1` FOREIGN KEY (`id_ficha`) REFERENCES `ficha` (`id_ficha`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `aprendizxficha_ibfk_2` FOREIGN KEY (`id_aprendiz`) REFERENCES `aprendiz` (`id_apre`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `asistencia`
+--
+ALTER TABLE `asistencia`
+  ADD CONSTRAINT `asistencia_ibfk_1` FOREIGN KEY (`id_actividad`) REFERENCES `actividad` (`id_act`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `ficha`
+--
+ALTER TABLE `ficha`
+  ADD CONSTRAINT `ficha_ibfk_1` FOREIGN KEY (`id_jor`) REFERENCES `jornada` (`id_jor`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `ficha_ibfk_2` FOREIGN KEY (`id_ficha`) REFERENCES `actividad` (`id_ficha`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `instructor`
+--
+ALTER TABLE `instructor`
+  ADD CONSTRAINT `instructor_ibfk_1` FOREIGN KEY (`usu_codigo`) REFERENCES `usuario` (`usu_codigo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `instructor_ficha`
+--
+ALTER TABLE `instructor_ficha`
+  ADD CONSTRAINT `instructor_ficha_ibfk_1` FOREIGN KEY (`id_ins`) REFERENCES `instructor` (`id_ins`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `instructor_ficha_ibfk_2` FOREIGN KEY (`id_ficha`) REFERENCES `ficha` (`id_ficha`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `rol_id` (`rol_id`) ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
