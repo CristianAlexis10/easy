d6b4a5d80ee770f7c1ac130f87ba66fc0bd8e694
julian <jdgiraldo08@misena.edu.co>
2018-06-18 11:56:55 -0500

    fdsgsfg

--- views/include/docente/scope.topMenu.php
+++ views/include/docente/scope.topMenu.php
@@ -1,6 +1,6 @@
 <div class="topMenu">
   <ul>
     <a href="#"><li><i class="fas fa-home"></i></li></a>
-    <a href="#"><li><i class="fas fa-sign-out-alt"></i></li></a>
+    <a href="finalizar"><li><i class="fas fa-sign-out-alt"></i></li></a>
   </ul>
 </div>
---
+++ views/modules/docente/asistance/index.php
@@ -0,0 +1,33 @@
+<div class="contentUser">
+  <div class="title">
+    <h1>asistencia</h1>
+  </div>
+    <form class="formActivity" action="index.html" method="post">
+      <div class="wrapInput">
+        <input required type="text" name="" value="">
+        <label for="">actividad</label>
+      </div>
+      <div class="wrapDate">
+        <div class="wrapInput">
+          <input required type="text" name="" value="">
+          <label for="">hora inicio</label>
+        </div>
+        <div class="wrapInput">
+          <input required type="text" name="" value="">
+          <label for="">hora fin</label>
+        </div>
+        <div class="wrapInput">
+          <input required type="text" name="" value="">
+          <label for="">fecha</label>
+        </div>
+      </div>
+      <div class="wrapInput">
+        <select class="" name="">
+          <option value="">fichas</option>
+        </select>
+      </div>
+      <div class="wrapInput">
+        <button type="submit" name="button">guardar</button>
+      </div>
+    </form>
+  </div>
